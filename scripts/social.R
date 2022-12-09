e1c2a7a11f00df38fba1e08392ddd1024823d4c3
khanspers <kristina.hanspers@gladstone.ucsf.edu>
2022-12-09 15:39:35 -0800

    minor change

--- scripts/social.R
+++ scripts/social.R
@@ -9,12 +9,15 @@ library(tidyverse)
 ###############################
 ## Read in yaml files from inbox
 setwd("/Users/kristinahanspers/Dropbox (Gladstone)/Work/github/wikipathways-pmcpaperbot/inbox")
+# setwd("inbox") 
 files <- list.files(pattern = "\\.yml$")
 
 ## for testing
 #testfile = "PMC9616486__gr4_lrg.yml"
 
 yaml.df <- data.frame()
+
+##TO-DO: Consider a smarter way of picking a subset of keywords, for example check against a list
 
 for (f in files){
   yaml <- read_yaml(f)
@@ -23,7 +26,7 @@ for (f in files){
   citation <- yaml[[3]]
   doi <- paste("https://doi.org/",yaml[[4]], sep="")
   journal <- yaml[[5]]
-  keywords <- formatKeywords(yaml$keywords[1:3]) ##store only first three and formatå
+  keywords <- formatKeywords(yaml$keywords[1:3]) ##store only first three and format
   yaml.df <- rbind(yaml.df, data.frame(image_filename, article_title, doi, keywords))
 }
    
@@ -31,20 +34,21 @@ for (f in files){
 ## Construct tweet / toot
 
 social.df <- yaml.df %>%
-  dplyr::mutate(status = paste(article_title, doi, keywords, sep = '\n'))
+  dplyr::mutate(status = as.character(paste(article_title, doi, keywords, sep = "\n")))
 
 ###############################
 ## Post to social (Mastodon)
 
+##TO-DO: Authentication
 #Authentication
 #auth_setup()
 
 #Loop through all yaml/image file
 for (i in 1:nrow(social.df)){
-  print(social.df$Status)
+  print(social.df$status)
   
   ## post toot
-  #post_toot(status,media = social.df$image_filename,alt_text = "network image")
+  #post_toot(status, media = social.df$image_filename, alt_text = "network image")
 
 }
 
