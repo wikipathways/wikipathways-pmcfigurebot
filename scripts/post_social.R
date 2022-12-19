6d88e7f7c8058b2f90378722b17644097280ab92
Alexander Pico <apico@gladstone.ucsf.edu>
2022-12-18 17:53:13 -0800

    updates to df handling

--- scripts/post_social.R
+++ scripts/post_social.R
@@ -2,55 +2,47 @@
 
 library(dplyr)
 library(yaml)
-install.packages("rtoot")
 library(rtoot)
-install.packages("tidyverse")
-library(tidyverse)
+library(stringr)
+
+###############################
+## FUNCTIONS 
+
+formatKeywords <- function(keywords){
+  kw <- paste("#",keywords, sep ="") ##Add hashtag
+  kw <- stringr::str_to_title(kw)  ##First letter uppercase
+  kw <- gsub(" ","", kw, ignore.case = T) ##remove spaces
+  kw <- paste(kw, collapse=' ')
+  return(kw)
+}
 
 ###############################
 ## Read in yaml files from inbox
+
 files <- list.files("./inbox", pattern = "\\.yml$")
 
 ## for testing
 #testfile = "PMC9616486__gr4_lrg.yml"
 
-yaml.df <- data.frame()
-
-##TO-DO: Consider a smarter way of picking a subset of keywords, for example check against a list
+##TODO: Consider a smarter way of picking a subset of keywords, for example check against a list
 
 for (f in files){
-  yaml <- read_yaml(f)
-  image_filename <- yaml[[1]]
-  article_title <- yaml[[2]]
-  citation <- yaml[[3]]
-  doi <- paste("https://doi.org/",yaml[[4]], sep="")
-  journal <- yaml[[5]]
-  keywords <- formatKeywords(yaml$keywords[1:3]) ##store only first three and format
-  yaml.df <- rbind(yaml.df, data.frame(image_filename, article_title, doi, keywords))
-}
-   
-###############################
-## Construct tweet / toot
-
-social.df <- yaml.df %>%
-  dplyr::mutate(status = as.character(paste(article_title, doi, keywords, sep = "\n")))
-
-###############################
-## Post to social (Mastodon)
-
-##TO-DO: Authentication
-#Authentication
-#auth_setup()
-
-#Loop through all yaml/image file
-for (i in 1:nrow(social.df)){
-  print(social.df$status)
+  social.df <- as.data.frame(yaml::read_yaml(file.path("./inbox",f)))[1:3,] %>%
+    dplyr::group_by(across(c(-keywords))) %>%
+    dplyr::summarise(keywords = formatKeywords(keywords)) %>%
+    dplyr::mutate(doi = paste0("https://doi.org/",doi)) %>%
+    dplyr::mutate(status = as.character(paste(article_title, doi, keywords, sep = "\n"))) %>%
+    as.data.frame()
   
+  print(social.df$status)
+    
   ## post toot
-  #post_toot(status, media = social.df$image_filename, alt_text = "network image")
-
+  ##TODO: Authentication
+  #Authentication
+  #auth_setup()
+  rtoot::post_toot(status, media = social.df$image_filename, alt_text = "network image")
 }
-
+ 
 ###############################
 ## Move to outbox
 
@@ -59,12 +51,4 @@ for (f in list.files("./inbox", pattern=".jpeg|.yml")){
             to = file.path("./outbox",f))
 }
 
-################################
 
-formatKeywords <- function(keywords){
-  kw <- paste("#",keywords, sep ="") ##Add hashtag
-  kw <- str_to_title(kw)  ##First letter uppercase
-  kw <- gsub(" ","", kw, ignore.case = T) ##remove spaces
-  kw <- paste(kw, collapse=' ')
-  return(kw)
-}
